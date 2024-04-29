f23c73cc72891bbc1b41270fb89cb07fc1ee439e
penykusuma <162266637+penykusuma@users.noreply.github.com>
2024-04-29 22:13:50 +0700

    backend 2
    request&respons

--- index.js
+++ index.js
@@ -6,6 +6,38 @@ app.get('/', (req, res) => {
     res.send('WELCOME')
 });
 
+// req.params
+app.get('/mahasiswa/:nim/:semester', (req, res) => {
+    const nim = req.params.nim;
+    const semester = req.params.semester;
+    res.send(`NIM: ${nim} SEMESTER: ${semester}`);
+});
+
+// req.Query
+app.get('/mahasiswa', (req, res) => {
+    const nim = req.query.nim;
+    const semester = req.query.semester;
+    res.send(`NIM: ${nim} SEMESTER: ${semester}`);
+});
+
+// req.body
+app.use(express.json());
+
+app.post('/mahasiswa', (req, res) => {
+    const nim = req.body.nim;
+    const semester= req.body.semester;
+    const angkatan= req.body.angkatan;
+    const prodi = req.body.prodi;
+
+    //proses penyimpanan data mahasiswa
+    res.send(`NIM: ${nim}
+            SEMESTER: ${semester}
+            ANGKATAN: ${angkatan}
+            PRODI: ${prodi}`);
+    
+});
+
+
 app.post('/', (req, res) => {
     res.send('Post data')
 });
